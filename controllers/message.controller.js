9878004cab41f8686c9f76d8a58cbaf9a92ab2ec
Cliff Su <stu01509@yahoo.com.tw>
2021-07-22 14:32:32 +0800

    feat: finish etf search adn favorite features

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,24 +1,39 @@
 const etf = require('../services/etf');
+const UserSchema = require('../database/user');
 const etfMessageTemplate = require('../utils/messageTemplate');
 
 module.exports = async function onMessage(event) {
-  const { text } = event.message;
-  if (text === '全部 ETF 查詢') {
-    const etfInfo = await etf.getEtfInfo();
-    const etfMessage = await etfMessageTemplate.etfMessageTemplate('全部 ETF 查詢', etfInfo);
-    event.reply(etfMessage);
-  }
-  if (text === 'ETF 投信查詢') {
-    event.reply(await etfMessageTemplate.investmentTrustSelectMessage());
-  }
-  if (text.match(/^[0-9]{4,}[a-zA-z]?$/gm)) {
-    const etfInfo = (await etf.getEtfInfo()).filter((item) => item.a === text.toUpperCase());
-    const etfMessage = await etfMessageTemplate.etfMessageTemplate(`${text} 查詢`, etfInfo);
-    if (etfInfo.length === 0) {
-      event.reply(`抱歉, 目前沒有代號 ${text} 的 ETF.`);
-    }
-    if (etfInfo.length !== 0) {
+  try {
+    const { text } = event.message;
+    const { userId } = event.source;
+
+    if (text === '全部 ETF 查詢') {
+      const etfInfo = await etf.getEtfInfo();
+      const etfMessage = await etfMessageTemplate.etfMessageTemplate('全部 ETF 查詢', etfInfo);
       event.reply(etfMessage);
     }
+    if (text === 'ETF 投信查詢') {
+      event.reply(await etfMessageTemplate.investmentTrustSelectMessage());
+    }
+    if (text === '自選股查詢') {
+      const result = await UserSchema.findOne({ userId }).select('-_id favoriteEtf').exec();
+      const etfInfo = (await etf.getEtfInfo())
+        .filter((item) => result.favoriteEtf.includes(item.a));
+
+      const etfMessage = await etfMessageTemplate.etfMessageTemplate('自選股查詢', etfInfo);
+      event.reply(etfMessage);
+    }
+    if (text.match(/^[0-9]{4,}[a-zA-z]?$/gm)) {
+      const etfInfo = (await etf.getEtfInfo()).filter((item) => item.a === text.toUpperCase());
+      if (etfInfo.length === 0) {
+        event.reply(`抱歉, 目前沒有代號 ${text} 的 ETF. 😭`);
+      }
+      if (etfInfo.length !== 0) {
+        const etfMessage = await etfMessageTemplate.singleEtfMessageTemplate(`${text} ${etfInfo[0].b}`, etfInfo);
+        event.reply(etfMessage);
+      }
+    }
+  } catch (error) {
+    event.reply('目前發生了點問題, 請稍後再試. 😭');
   }
 };
--- controllers/postback.controller.js
+++ controllers/postback.controller.js
@@ -1,21 +1,48 @@
+const UserSchema = require('../database/user');
 const etf = require('../services/etf');
 const etfMessageTemplate = require('../utils/messageTemplate');
 
 module.exports = async function onPostBack(event) {
-  // postback[0] 搜尋類型
-  // postback[1] 發行投信
-  // postback[2] 起始索引
-  const searchMode = event.postback.data.split('&')[0];
-  const brand = event.postback.data.split('&')[1];
-  const index = event.postback.data.split('&')[2];
-  if (searchMode === 'all') {
-    const etfInfo = await etf.getEtfInfo();
-    const etfMessage = await etfMessageTemplate.etfMessageTemplate('全部 ETF 查詢', etfInfo, Number.parseInt(index, 10));
-    event.reply(etfMessage);
-  }
-  if (searchMode === 'investmentTrust') {
-    const etfInfo = (await etf.getEtfInfo()).filter((item) => item.b.includes(brand));
-    const etfMessage = await etfMessageTemplate.etfMessageTemplate(`${brand} ETF 查詢`, etfInfo);
-    event.reply(etfMessage);
+  try {
+    // postback[0] 搜尋類型
+    // postback[1] 發行投信/股票代號
+    // postback[2] 起始索引
+    const { userId } = event.source;
+    const mode = event.postback.data.split('&')[0];
+    const brand = event.postback.data.split('&')[1];
+    const index = event.postback.data.split('&')[2];
+
+    if (mode === 'all') {
+      const etfInfo = await etf.getEtfInfo();
+      const etfMessage = await etfMessageTemplate.etfMessageTemplate('全部 ETF 查詢', etfInfo, Number.parseInt(index, 10));
+      event.reply(etfMessage);
+    }
+    if (mode === 'investmentTrust') {
+      const etfInfo = (await etf.getEtfInfo()).filter((item) => item.b.includes(brand));
+      const etfMessage = await etfMessageTemplate.etfMessageTemplate(`${brand} ETF 查詢`, etfInfo);
+      event.reply(etfMessage);
+    }
+    if (mode === 'etfAdd') {
+      const result = await UserSchema.findOne({ userId }).select('-_id favoriteEtf').exec();
+      if (result.favoriteEtf.includes(brand)) {
+        event.reply('已經新增過囉！');
+      } else if (result.favoriteEtf.length > 24) {
+        event.reply('已經達到新增上限囉！');
+      } else {
+        await UserSchema.findOneAndUpdate({ userId }, { $push: { favoriteEtf: brand } }).exec();
+        event.reply(`新增 ${brand} 成功！ 🎉🎉🎉`);
+      }
+    }
+    if (mode === 'etfRemove') {
+      const result = await UserSchema.findOne({ userId }).select('-_id favoriteEtf').exec();
+      if (result.favoriteEtf.includes(brand)) {
+        await UserSchema.findOneAndUpdate({ userId }, { $pull: { favoriteEtf: brand } }).exec();
+        event.reply(`移除 ${brand} 成功！ 🎉🎉🎉`);
+      } else {
+        event.reply(`您沒有收藏 ${brand} ETF`);
+      }
+    }
+  } catch (error) {
+    event.reply('目前發生了點問題, 請稍後再試. 😭');
   }
 };
